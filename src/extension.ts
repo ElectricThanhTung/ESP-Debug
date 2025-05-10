8a6ea84ecaca6cfbd10468898b5523fa41d33cb0
Thanh Tùng <electricthanhtung@gmail.com>
2025-05-10 11:55:57 +0700

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('esp-debug', () => {
+		
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+
+}
